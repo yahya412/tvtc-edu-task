849fd8113c321b840cbb9848d825bd493190f072
yahya412 <yahya412@yahoo.com>
2020-10-29 17:17:53 +0300

    added the gender page .. and test data

---
+++ app/Http/Controllers/genderController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\gender;
+use Illuminate\Support\Facades\Auth;
+
+class genderController extends Controller
+{
+   function editGender(){
+       
+       if (Auth::user()){
+      /*  $user = Auth::user();*/ 
+       return view('gender')->with('user',auth()->user());
+       }
+   }
+   function updateGender(Request $request){
+       
+       $user = Auth::user();
+       $user->update([
+           'gender'=> $request->gender
+           
+       ]);
+       dd($user);
+       /*Session::flash('alert-class','gender updated successfuly');
+       $request->session()->flash('status', 'Task was successful!');*/
+       return view('dashboard');
+       
+   }
+}
