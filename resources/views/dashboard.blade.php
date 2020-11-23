8401e23ef5b52941edb5851fb7fd45e2bb92bb89
yahya412 <yahya412@yahoo.com>
2020-11-23 03:18:52 +0300

    small screen

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,8 +22,8 @@
 
 
 
-    <div class="w-10/12 my-10 flex">
-        <div class="w-5/12 rounded border p-2">
+    <div class="w-10/12 my-10 ">
+        <div class="w-5/12 rounded border p-2 col-xs-12">
 
             @if(Auth::user()->gender == "1")
             <h2>Men only</h2>
@@ -32,7 +32,7 @@
             <h2>Women only</h2>
             <p class="div-women">Dear Ladies, I feel glad to call you people my family. Getting to celebrate this unique day with you is nothing but a pure blessing from the Lord. Happy family day.</p>
             @endif</div>
-        <div class="w-7/12 mx-2 rounded border p-2">
+        <div class="w-7/12 mx-2 col-xs-12 rounded border p-2">
             <livewire:posts />
         </div>
     </div>
